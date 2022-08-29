f9b3eeb167660ab3581e90a1852d3a32dc95cef4
Anuj <anujgupta7906@gmail.com>
2022-08-29 13:56:21 +0530

    working on fetching and setting the data

--- tour_of_india/src/App.js
+++ tour_of_india/src/App.js
@@ -1,8 +1,32 @@
+import React,{useState} from 'react';
+import Tour from './Tour';
+import Loading from './Loading';
+import Data from './Data';
 
 function App() {
-  return (
-    <div>Tours</div>
-  );
+  const [loading,setLoading] = useState(true);
+  const [tour,setTour] = useState([]);
+
+  const fetchTours = async()=>{
+    setLoading(true);
+
+    try {
+      setLoading(false);
+      setTour(Data);
+
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }
+  
+  if(loading){
+    return(
+      <main>
+        <Loading />
+      </main>
+    )
+  }
 }
 
 export default App;
